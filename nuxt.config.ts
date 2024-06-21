7aa9827130c76b388de1a84e946ba6f8df774aa8
Nahuel Daima <nahueldaima@gmail.com>
2024-06-21 18:17:23 +0200

    Added robots.txt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,10 +11,6 @@ export default defineNuxtConfig({
     pageTransition: { name: 'page', mode: 'out-in' },
     layoutTransition: { name: 'layout', mode: 'out-in' },
   },
-
-  sitemap: {
-    strictNuxtContentPaths: true,
-  },
   site: {
     url: 'https://nahueldaima.com',
     identity: {
@@ -65,7 +61,6 @@ export default defineNuxtConfig({
     '@nuxtjs/robots',
     '@nuxtjs/fontaine',
     '@nuxtjs/color-mode',
-    'nuxt-simple-sitemap',
     '@nuxtjs/tailwindcss',
     '@stefanobartoletti/nuxt-social-share',
     '@nuxtjs/i18n',
